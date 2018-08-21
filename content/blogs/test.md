7c7790726eb6bd001ad8a46ea724bdd56049b1d6
ali <invisement@gmail.com>
2018-08-21 15:10:24 +0000

    testing charts

---
+++ content/blogs/test.md
@@ -0,0 +1,21 @@
+---
+title: test
+
+---
+
+
+
+<div id="test-div"> </div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/echarts/4.1.0/echarts-en.min.js" integrity="sha256-x8R4YOgRkrX/cMbupOzglWi/gSM/WD8bfFHrE+r5TPM=" crossorigin="anonymous"></script>
+<script src="/js/charts.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+
+
+<script> func(); </script>
+
+
+THis is a test.
+
+
--- content/js/charts.js
+++ content/js/charts.js
@@ -63,4 +63,51 @@ riskGauge_option = {
 
     ]
 };
+
+/////////           FRED data fetch and chart       /////
+myFredApiKey = "88b9092ad3db013e454ea78d5a1084c9"
+high_yield_series = "BAMLH0A0HYM2EY"
+treasury_spread_series = "T10Y2Y"
+fred_endpoint = "http://api.stlouisfed.org/fred/series/observations"
+fred_file_type = "json"
+
+
+//today = new Date()
+//fred_realtime_start = (today.getFullYear()-30) + '-' + (today.getMonth()+1) + '-' + today.getDate()
+//a = today.toISOString()
+
+
+// https://api.stlouisfed.org/fred/series?series=T10Y2Y&api_key=88b9092ad3db013e454ea78d5a1084c9&file_type=json
+
+function fetch_fred () {
+    url = fred_endpoint + "?series_id=" + treasury_spread_series + "&api_key=" + myFredApiKey + "&file_type=" + fred_file_type
+
+    fetch("https://google.com", {mode: 'cors'})
+        //.then(res => res.json())
+        .then(function(x){
+            document.getElementById("test-div").innerHTML = "hello";
+        })
+        .catch(function(x){
+            document.getElementById("test-div").innerHTML = x;
+            
+        });
+
+    //document.getElementById("test-div").innerHTML = url;
+}
+
+
+function func () {
+    document.getElementById("test-div").innerHTML = "first"
+
+    $.ajax({
+        url: "http://google.com"
+    })
+    .done(function(){
+        document.getElementById("test-div").innerHTML = "done"
+    }).fail(function(error){
+        document.getElementById("test-div").innerHTML = error
+    });
+    document.getElementById("test-div").innerHTML = "skipped"
+ 
+}
 /////////           END         ///////
